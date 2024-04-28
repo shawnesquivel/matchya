8a94dfec7640f1cf458c21c231aa23f5647f8d6d
Shawn Esquivel <shawnesquivel24@gmail.com>
2024-04-28 12:01:54 -0700

    cleanup backend

--- server/app.py
+++ server/app.py
@@ -17,7 +17,55 @@ cors_config = CORSConfig(
 
 @app.route("/")
 def index():
-    return {"hello": "world!"}
+    """
+    Verify the server status by going to http://127.0.0.1:8000/
+
+    Documentation: https://aws.github.io/chalice/quickstart.html
+    """
+    return {"Welcome": "to the future!"}
+
+
+@app.route("/hello/{name}")
+def hello_name(name):
+    """
+    Receives the value after the /hello/{VALUE} does something to it.
+
+    Examples
+    --------
+    http://127.0.0.1:8000/hello/elon
+
+    http://127.0.0.1:8000/hello/sam
+    """
+    # '/hello/james' -> {"hello": "james"}
+
+    return {"message": f"Welcome to the course, {name.upper()}!"}
+
+
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
+
+
+@app.route("/kitsune", methods=["POST"], cors=cors_config)
+def kitsune_chatbot():
+    try:
+        user_message = app.current_request.json_body["message"]
+        user_api_key = app.current_request.json_body["apiKey"]
+        return Response(
+            body={
+                "data": {"response": f"Success: {user_message} api key {user_api_key}"}
+            },
+            status_code=200,
+        )
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+        return Response(body={"error": str(e)}, status_code=500)
 
 
 @app.route("/chat", methods=["POST", "GET"], cors=cors_config)
@@ -59,7 +107,7 @@ def tutorial_chat_endpoint():
 
 
 @app.route("/agents", methods=["POST", "GET"], cors=cors_config)
-def tutorial_chat_endpoint():
+def agents_endpoint():
     """
     Test endpoint for NextJS tutorial.
 
@@ -84,24 +132,3 @@ def tutorial_chat_endpoint():
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
         return Response(body={"error": str(e)}, status_code=500)
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
