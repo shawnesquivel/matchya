5a56e7a2e5014657e37c124d5d32d8de13c3256b
Shawn Esquivel <shawnesquivel24@gmail.com>
2024-05-01 14:20:02 -0700

    phase 2: add the messages history from dynamodb, and format for openai api

--- server/chalicelib/kitsune_1_chatbot.py
+++ server/chalicelib/kitsune_1_chatbot.py
@@ -1,18 +1,56 @@
 from openai import OpenAI
+from chalicelib.update_table import get_all_messages_for_chat
 
 # hardcode this for now
 # client = OpenAI(api_key="")
-client = OpenAI(api_key="")
+client = OpenAI(api_key="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")
+
+
+# def send_message_to_openai(user_message, prompt_template, model, temperature):
+#     """PHASE 1: Make a call to OpenAI and return the latest message."""
+
+#     print(f"Received request {user_message} {prompt_template} {model} {temperature}")
+
+#     try:
+
+#         system_prompt = fetch_system_prompt(prompt_template)
+
+#         response = client.chat.completions.create(
+#             # We pass the model here, the list of models can be found here:
+#             # https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
+#             model=model,
+#             temperature=temperature,
+#             # The messages must have this format: https://platform.openai.com/docs/api-reference/chat/create
+#             messages=[
+#                 {
+#                     "role": "system",
+#                     "content": system_prompt,
+#                 },
+#                 # Put the latest message here.
+#                 # new messages will be added to the end
+#                 {"role": "user", "content": user_message},
+#             ],
+#         )
+#         print(f"Full Response: {response}")
+#         # This will always pass back an 'assistant' message
+#         last_message = response.choices[0].message.content
+#         print("last message", last_message)
+
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+#         last_message = f"An error occurred: {str(e)}"
+#     return last_message
 
 
 def send_message_to_openai(user_message, prompt_template, model, temperature):
-    """Make a call to OpenAI and return the latest message."""
-
-    print(f"Received request {user_message} {prompt_template} {model} {temperature}")
+    """PHASE 2: Duplicate the last send_message_to_openai and fetch old messages instead"""
 
     try:
 
-        system_prompt = fetch_system_prompt(prompt_template)
+        """
+        We replace the message history with this.
+        """
+        messages = create_messages_openai(prompt_template, user_message)
 
         response = client.chat.completions.create(
             # We pass the model here, the list of models can be found here:
@@ -20,17 +58,9 @@ def send_message_to_openai(user_message, prompt_template, model, temperature):
             model=model,
             temperature=temperature,
             # The messages must have this format: https://platform.openai.com/docs/api-reference/chat/create
-            messages=[
-                {
-                    "role": "system",
-                    "content": system_prompt,
-                },
-                # Put the latest message here.
-                {"role": "user", "content": user_message},
-                # new messages will be added to the end
-            ],
+            messages=messages,
         )
-
+        print(f"Full Response: {response}")
         # This will always pass back an 'assistant' message
         last_message = response.choices[0].message.content
         print("last message", last_message)
@@ -110,6 +140,39 @@ def fetch_system_prompt(prompt_template: str) -> str:
         """
             + suffix
         )
+
+
+def create_messages_openai(prompt_template, user_message):
+    """PHASE 2: Fetches the old messages and adds the conversation history"""
+
+    system_prompt = fetch_system_prompt(prompt_template)
+
+    messages = [
+        {
+            "role": "system",
+            "content": system_prompt,
+        },
+        # we need to add the old messages here
+        # then the new messages
+    ]
+
+    # mock conversaiton history from dynamdodb
+    old_messages = get_all_messages_for_chat("o8045x2mfcapik2b6ebzu")
+
+    for old_message in old_messages:
+        # print(old_message)
+        # print(f"old_message: {old_message['content']} {old_message['role']}")
+
+        # we dont need the full response, just the role and content, for OpenAI
+        messages.append(
+            {"role": old_message["role"], "content": old_message["content"]}
+        )
+
+    # added the latest user message
+    messages.append({"role": "user", "content": user_message})
+
+    print(f"Number of messages{len(messages)}")
+    return messages
 
 
 if __name__ == "__main__":
