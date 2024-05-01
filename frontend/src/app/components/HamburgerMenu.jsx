80b93eb547d46fde72b402540fa766d7b4ce2b5b
Shawn Esquivel <shawnesquivel24@gmail.com>
2024-05-01 11:45:58 -0700

    fix styling

--- frontend/src/app/components/HamburgerMenu.jsx
+++ frontend/src/app/components/HamburgerMenu.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import { pressStart2P, sourceCodePro } from "../styles/fonts";
+import "./hamburgerMenu.css";
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -4,23 +4,27 @@ import React from "react";
 import Link from "next/link";
 import HamburgerMenu from "./HamburgerMenu";
 const Navbar = () => {
-  // const Navbar = () => {
   return (
     <nav className="fixed z-10 top-0 bg-gray-50 text-gray-800 w-full p-4 grid grid-cols-3 items-center">
       <a href="/" className={`text-center`}>
-        AI-41
+        LANGCHAIN JS UDEMY COURSE
       </a>
       <HamburgerMenu />
       <p className={`text-center`}>WEEKNIGHTS + WEEKENDS</p>
       <div className="hidden">
         <Link href="/">Home 🏡 </Link>
-        {/* Projects */}
+
         <Link href="/pdf">PDF-GPT 👨🏻‍🏫</Link>
         <Link href="/memory">Memory 🧠</Link>
         <Link href="/streaming">Streaming 🌊</Link>
+
+        {/* Documents / QA */}
         <Link href="/transcript-qa">YouTube Video Chat 💬</Link>
+
         {/* APIs, Templates, Agents */}
         <Link href="/content-generator">AI Content Wizard 🧙🏼</Link>
+
+        {/* Documents / Database */}
         <Link href="/resume-reader">RoboHR 🤖</Link>
         <Link href="/api-tester">Testing ⚠️</Link>
       </div>
