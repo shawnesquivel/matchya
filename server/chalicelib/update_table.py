a56641c362fedf68c561d5c34a24aafba6d4d517
Shawn Esquivel <shawnesquivel24@gmail.com>
2024-05-05 12:49:15 -0700

    comment examples

--- server/chalicelib/update_table.py
+++ server/chalicelib/update_table.py
@@ -60,5 +60,5 @@ def get_all_messages_for_chat(chat_id):
 
 
 # Example
-store_message("abc123", "hello world", 'user', 1234, None)
-print(get_all_messages_for_chat("abc123"))
+# store_message("abc123", "hello world", 'user', 1234, None)
+# print(get_all_messages_for_chat("abc123"))
