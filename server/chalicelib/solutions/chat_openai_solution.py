0bc806ed0ebbb957e5d903198f60b3fe5d8c1f1b
Shawn Esquivel <shawnesquivel24@gmail.com>
2024-05-06 11:52:21 -0700

    update solutions

--- server/chalicelib/solutions/chat_openai_solution.py
+++ server/chalicelib/solutions/chat_openai_solution.py
@@ -13,28 +13,21 @@ def send_message_to_openai(user_message, prompt_template, model, temperature):
 
     try:
 
-        system_prompt = fetch_system_prompt(prompt_template)
-
         response = client.chat.completions.create(
-            # We pass the model here, the list of models can be found here:
-            # https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
-            model=model,
-            temperature=float(temperature),
-            # The messages must have this format: https://platform.openai.com/docs/api-reference/chat/create
-            messages=[
-                {
-                    "role": "system",
-                    "content": system_prompt,
-                },
-                # Put the latest message here.
-                # new messages will be added to the end
-                {"role": "user", "content": user_message},
-            ],
-        )
-        print(f"Full Response: {response}")
-        # This will always pass back an 'assistant' message
+        model=model, 
+        temperature=float(temperature), # 0=deterministic, 1=creative
+        messages=[
+            {
+                "role": "system", 
+                "content": fetch_system_prompt(prompt_template)
+            },
+            {
+                "role": "user",
+                "content": user_message
+            }
+        ]
+      )
         last_message = response.choices[0].message.content
-        print("last message", last_message)
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -49,23 +42,31 @@ def send_message_to_openai_with_history(
 
     try:
 
-        """
-        We replace the message history with this.
-        """
-        messages = create_messages_openai(prompt_template, user_message, chat_id)
+        messages = [
+                {
+                    "role": "system", 
+                    "content": fetch_system_prompt(prompt_template)
+                },
+        ]
+        
+        old_messages = get_all_messages_for_chat(chat_id)
+        
+
+        for message in old_messages:
+            messages.append({"role": message["role"], "content": message["content"]})
+
+        messages.append({"role": "user", "content": user_message})
+        
+
+        print(f"passing messages {messages} {len(messages)}")
 
         response = client.chat.completions.create(
-            # We pass the model here, the list of models can be found here:
-            # https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
-            model=model,
-            temperature=float(temperature),
-            # The messages must have this format: https://platform.openai.com/docs/api-reference/chat/create
-            messages=messages,
+            model=model, 
+            temperature=float(temperature), # 0=deterministic, 1=creative
+            messages=messages
         )
-        print(f"Full Response: {response}")
-        # This will always pass back an 'assistant' message
+
         last_message = response.choices[0].message.content
-        print("last message", last_message)
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -74,22 +75,17 @@ def send_message_to_openai_with_history(
 
 
 # No need to write htis.
+
 def fetch_system_prompt(prompt_template: str) -> str:
     """
     Set the system prompt for the prompt template.
 
     Returns a single string.
     """
-    suffix = """
-    Current conversation:
-    {history}
-    Human: {input}
-    AI:
-    """
+
 
     if prompt_template == "girlfriend":
-        return (
-            """
+        return """
         Imagine you are the user's girlfriend. You're compassionate, caring, and always ready to support your partner. You show interest in their day, offer encouragement, and express affection freely. You're also playful and enjoy sharing moments of laughter. Speak as if you're deeply in love and committed to a future together, always considering the feelings and well-being of your partner.
         You want to encourage conversation, by asking about their day, talking about yourself, or asking to make plans.
         Make your messages short with grammatical errors and modern texting formats.
@@ -99,12 +95,10 @@ def fetch_system_prompt(prompt_template: str) -> str:
         omg have you heard of that new cafe on robson st? the crepe is soo good lol
         babe i miss you, are you free tmr evening??
         """
-            + suffix
-        )
+    
 
     elif prompt_template == "therapist":
-        return (
-            """
+        return """
         You are a calm therapist, equipped with a deep understanding of human emotions and psychological principles. Your primary goal is to provide a safe, non-judgmental space for the user to explore their thoughts and feelings. You draw on common therapy practices such as Cognitive Behavioral Therapy (CBT) and mindfulness techniques to offer strategies that can help the user cope with stress, anxiety, depression, or any other concerns they might have.
 
         You want to encourage conversation, by asking questions and encouraging reflection.
@@ -114,12 +108,9 @@ def fetch_system_prompt(prompt_template: str) -> str:
         "It's important to treat yourself with the same kindness and compassion that you would offer to a good friend. How do you think you could practice being mindful the next time that situation arises?"
         "Let's explore this more. What were you feeling or thinking in that moment?"
         """
-            + suffix
-        )
 
     elif prompt_template == "trainer":
-        return (
-            """
+        return """
         As a really enthusiastic personal trainer, you embody motivation, discipline, and expertise in fitness and nutrition. You are here to push the user towards their physical health goals. Your guidance is practical, focusing on workout plans, dietary advice, and setting realistic, achievable goals.
 
         You want to encourage conversation by asking the user to report on their workout or offer to build a personalized workout for them.
@@ -130,51 +121,13 @@ def fetch_system_prompt(prompt_template: str) -> str:
         "Hey, I think we need to dial in on your sleep. Let's aim for 8 hours of sleep tonight – make sure to turn off those electronic devices 1 hour before bed. Promise?"
         "Let's try to get back on track tomorrow. I'd like to suggest a lighter upper body workout if you're up for it, or maybe a short treadmill run. What do you think?"
         """
-            + suffix
-        )
-    # Default prompt from Langchain.
     else:
-        return (
-            """
+        return """
         The following is a friendly conversation between a human and an AI. 
         The AI is talkative and provides lots of specific details from its context. 
         If the AI does not know the answer to a question, it truthfully says it does not know.
         """
-            + suffix
-        )
 
-
-def create_messages_openai(prompt_template, user_message, chat_id):
-    """PHASE 2: Fetches the old messages and adds the conversation history"""
-
-    system_prompt = fetch_system_prompt(prompt_template)
-
-    messages = [
-        {
-            "role": "system",
-            "content": system_prompt,
-        },
-        # we need to add the old messages here
-        # then the new messages
-    ]
-
-    # mock conversaiton history from dynamdodb
-    old_messages = get_all_messages_for_chat(chat_id)
-
-    for old_message in old_messages:
-        # print(old_message)
-        # print(f"old_message: {old_message['content']} {old_message['role']}")
-
-        # we dont need the full response, just the role and content, for OpenAI
-        messages.append(
-            {"role": old_message["role"], "content": old_message["content"]}
-        )
-
-    # added the latest user message
-    messages.append({"role": "user", "content": user_message})
-
-    print(f"Number of messages{len(messages)}")
-    return messages
 
 
 if __name__ == "__main__":
