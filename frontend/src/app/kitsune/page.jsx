689753201132d001ce9bea199f1140fe2cc2f896
Shawn Esquivel <shawnesquivel24@gmail.com>
2024-05-06 13:46:21 -0700

    production instance for vercel

--- frontend/src/app/kitsune/page.jsx
+++ frontend/src/app/kitsune/page.jsx
@@ -1,11 +1,25 @@
 "use client";
 import React from "react";
-
+import Chatbot from "../layouts/Chatbot";
 const Kitsune = () => {
   /**
    * Phase 1: Chatbot
    */
-  return <></>;
+  return (
+    <>
+      <Chatbot
+        emoji="🦊"
+        headingText="Chapter 1"
+        heading="Kitsune AI"
+        boldText="Phase 1: Fully Customizable Chatbot"
+        description="Generative Text. Prompt Engineering. Few Shot Learning."
+        debug={true}
+        /** Base URL when using `chalice local` */
+        baseUrl="https://lg9eg36d44.execute-api.us-west-2.amazonaws.com/api"
+        botPngFile="girlfriend"
+      />
+    </>
+  );
 };
 
 export default Kitsune;
