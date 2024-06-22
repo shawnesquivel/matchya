f6701522e52ddde9f004831e5d911f93f21dcb4f
Shawn Esquivel <94336773+shawnesquivel@users.noreply.github.com>
2024-06-22 09:00:59 -0700

    Update to starter base URL

--- frontend/src/app/kitsune/page.jsx
+++ frontend/src/app/kitsune/page.jsx
@@ -15,7 +15,8 @@ const Kitsune = () => {
         description="Generative Text. Prompt Engineering. Few Shot Learning."
         debug={true}
         /** Base URL when using `chalice local` */
-        baseUrl="https://lg9eg36d44.execute-api.us-west-2.amazonaws.com/api"
+        basUrl="http://localhost:3000"
+        // baseUrl="https://lg9eg36d44.execute-api.us-west-2.amazonaws.com/api"
         botPngFile="girlfriend"
       />
     </>
