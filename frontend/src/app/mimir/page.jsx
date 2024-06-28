c98cb3cd2899a12ff9908482db5587e691158f77
Shawn Esquivel <shawnesquivel24@gmail.com>
2024-06-28 10:23:50 -0700

    trigger redeploy

--- frontend/src/app/mimir/page.jsx
+++ frontend/src/app/mimir/page.jsx
@@ -16,6 +16,7 @@ const Kitsune = () => {
         debug={false}
         /** Base URL when using `chalice local` */
         baseUrl="https://smvfn4yz5a.execute-api.us-west-2.amazonaws.com/api"
+        // trigger it
         // baseUrl="http://127.0.0.1:8000"
         botPngFile="wizard"
       />
