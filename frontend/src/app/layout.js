0d215e88a7811964d8b034a0548b8eabb2259db8
Shawn Esquivel <shawnesquivel24@gmail.com>
2024-07-13 11:03:22 -0700

    therapist profile: add placeholders

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,9 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
 import { aspekta } from "./styles/fonts";
-
 
 export const metadata = {
   title: "matchya",
@@ -17,9 +15,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script async src="https://tally.so/widgets/embed.js"></script></head>
+        <script async src="https://tally.so/widgets/embed.js"></script>
+      </head>
       <body className={`flex flex-col h-screen ${aspekta.className}`}>
-        <main className="bg-grey gap-2 flex flex-col px-2 pt-0 lg:gap-6 lg:px-20 md:px-10 h-full ">{children}</main>
+        <main className="bg-grey gap-2 flex flex-col px-2 pt-0 lg:gap-6 lg:px-20 md:px-10 h-full ">
+          {children}
+        </main>
         <Analytics />
         <Footer />
       </body>
