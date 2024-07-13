f859b282236ed1a78cf0d650aa255f760a637438
Shawn Esquivel <shawnesquivel24@gmail.com>
2024-07-13 12:10:50 -0700

    add clerk

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,6 +2,7 @@ import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import Footer from "./components/Footer";
 import { aspekta } from "./styles/fonts";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
   title: "matchya",
@@ -13,17 +14,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <script async src="https://tally.so/widgets/embed.js"></script>
-      </head>
-      <body className={`flex flex-col h-screen ${aspekta.className}`}>
-        <main className="bg-grey gap-2 flex flex-col px-2 pt-0 lg:gap-6 lg:px-20 md:px-10 h-full ">
-          {children}
-        </main>
-        <Analytics />
-        <Footer />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <script async src="https://tally.so/widgets/embed.js"></script>
+        </head>
+        <body className={`flex flex-col h-screen ${aspekta.className}`}>
+          <main className="bg-grey gap-2 flex flex-col px-2 pt-0 lg:gap-6 lg:px-20 md:px-10 h-full ">
+            {children}
+          </main>
+          <Analytics />
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
