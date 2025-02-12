c837e6c226df774fc30fe454629da4e8918ec6a2
Shawn Esquivel <shawnesquivel24@gmail.com>
2025-02-12 17:14:52 +0700

    remove server

--- server/chalicelib/utils_s3.py
+++
@@ -1,6 +0,0 @@
-import boto3
-
-def get_ssm_parameter(name):
-    ssm = boto3.client('ssm')
-    response = ssm.get_parameter(Name=name, WithDecryption=True)
-    return response['Parameter']['Value']
