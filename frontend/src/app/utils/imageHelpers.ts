515580dc102acd7823fc86ee4943c19e3b5c33c6
Shawn Esquivel <shawnesquivel24@gmail.com>
2025-03-20 13:05:31 +0700

    clean up warning

--- frontend/src/app/utils/imageHelpers.ts
+++ frontend/src/app/utils/imageHelpers.ts
@@ -2,28 +2,22 @@ import { ALLOWED_IMAGE_DOMAINS } from "@/config/images";
 
 /**
  * Returns a safe image URL for use in Image components.
- * Falls back to a placeholder if the provided URL is invalid or undefined.
  */
 export const getSafeImageUrl = (url: string | undefined | null): string => {
-  console.log("getSafeImageUrl called with URL:", url);
-  if (!url) {
-    console.warn("No URL provided, returning default image.");
-    return "/assets/images/default-pp.png";
-  }
+  if (!url) return "/assets/images/default-pp.png";
 
   try {
     const urlObj = new URL(url);
     const isAllowedDomain = ALLOWED_IMAGE_DOMAINS.includes(
       urlObj.hostname as any,
     );
-    console.log(
-      `URL hostname "${urlObj.hostname}" is ${
-        isAllowedDomain ? "allowed" : "not allowed"
-      }.`,
-    );
+    if (!isAllowedDomain) {
+      console.warn(
+        `URL hostname "${urlObj.hostname}" is not allowed. Check next.config.js and config/images.ts`,
+      );
+    }
     return isAllowedDomain ? url : "/assets/images/default-pp.png";
-  } catch (error) {
-    console.error("Error creating URL object:", error);
+  } catch {
     return "/assets/images/default-pp.png";
   }
 };
